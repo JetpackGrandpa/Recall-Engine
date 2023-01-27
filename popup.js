f1b507765a9a0b5751902a314e003527bbf07cf4
Narendra Prasath <83782340+JetpackGrandpa@users.noreply.github.com>
2023-01-27 10:58:59 +0530

    Add files via upload

--- popup.js
+++ popup.js
@@ -1,3 +1,42 @@
+import fetch from 'node-fetch';
+import mongoose from 'mongoose';
+import express from 'express';
+
+
+
+const postSchema = new mongoose.Schema({
+    playlist_title : {
+        type: String,
+        required: true
+    },
+    videos_cnt : {
+        type: String,
+        required: true
+    },
+    playlist_views : {
+        type: String,
+        required: true
+    },
+    vdo_data : {
+        type: Array,
+        required: true
+    }
+});
+
+
+
+
+mongoose.set('strictQuery',true);
+mongoose.connect("mongodb://127.0.0.1:27017/RecallE",{
+    useNewUrlParser : true,
+    useUnifiedTopology : true
+});
+
+
+
+
+const Post = mongoose.model('Post',postSchema);
+
 const options = {
 	method: 'GET',
 	headers: {
@@ -6,26 +45,48 @@ const options = {
 	}
 };
 
-document.getElementById("playIDb").addEventListener("click",getpID);
-function getpID(){
-    const playID = document.getElementById("playID").value;
-fetch(`https://youtube-search-and-download.p.rapidapi.com/playlist?id=${playID}`, options)
-	.then(response => response.json())                                
-	.then(data => {
-        const vcnt = data.videosCount;
-        const ptit = data.title;
-            const op1 = `<p>Total Videos: ${vcnt}<br>Playlist Title: ${ptit}</p>`
-            document.querySelector('.playlist').innerHTML += op1;
-        const list = data.contents;
-        list.map((item) => {
-            console.log(item)
-            const vId = item.video.videoId;
-            const vtit = item.video.title;
-            const vdur = item.video.lengthText;
-            const vthmb = item.video.thumbnails[0].url;
-            const op2 = `<ul><li><p>${vId}<br>${vtit}<br>${vdur}<br><img src="${vthmb}"></p></li><ul>`
-            document.querySelector('.playlist').innerHTML += op2;
-        })
-    })
-	.catch(err => console.error(err));
-}
+
+async function getpID(){
+    
+    const data = await fetch("https://youtube-search-and-download.p.rapidapi.com/playlist?id=PLpVg7pgd-JzPZwZ-4BzZY9vhHvuTkXgxN", options)
+    const res = await data.json();
+    console.log(res);
+
+    let vid_arr = new Array();
+    let vtit_arr = new Array();
+    let vdur_arr = new Array();
+    let vthmb_arr = new Array();
+
+
+    for(let i=0;i<(res.contents.length);i++){
+            vid_arr[i] = res.contents[i].video.videoId;
+            vtit_arr[i] = res.contents[i].video.title;
+            vdur_arr[i] = res.contents[i].video.lengthText;
+            vthmb_arr[i] = res.contents[i].video.thumbnails[0].url;
+    }
+
+    let temp_arr = new Array();
+    let vdo_data_arr = new Array();
+    //let emt_arr = [];
+
+    for(let j=0;j<(vid_arr.length);j++){
+        temp_arr.push(vid_arr[j]);
+        temp_arr.push(vtit_arr[j]);
+        temp_arr.push(vdur_arr[j]);
+        temp_arr.push(vthmb_arr[j]);
+        vdo_data_arr[j]=temp_arr;
+        temp_arr = [];
+        //console.log("temp_arr " + j + " :" + temp_arr + "\n");
+        // console.log("emt_arr " + j + " :" + emt_arr + "\n");
+    }
+
+        const post = new Post({
+            //con_tents : res.contents,
+            playlist_title : res.title,
+            videos_cnt : res.videosCount,
+            playlist_views : res.views,
+            vdo_data : vdo_data_arr
+            });
+    post.save();
+    
+}getpID();
